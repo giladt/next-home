2727a6eb3336db9088db7b669a4de67bd7e4e10a
Gilad Tsabar <gilad@tsabar.net>
2021-01-22 09:42:44 +0100

    fix material reference

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router'
-import { MuiThemeProvider } from 'material-ui/styles'
+import { MuiThemeProvider } from '@material-ui/core/styles'
 import { makeStyles } from '@material-ui/core/styles'
 
 import {
